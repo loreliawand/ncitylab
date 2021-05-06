aa6e5627c224ad0b9be119ddeb37ba8f38fcaf26
loreliawand <loreliawand@gmail.com>
2021-05-06 07:47:47 +0200

    Edited code for main app.

--- main.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const port = 8080;
-const server = http.createServer((req, res) => {
-  res.end('Future is coming');
-});
-server.listen(port, () => {
-  console.log('Server listening on: http://localhost:%s', port);
-});
