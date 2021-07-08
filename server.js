93ee34a978206d8b43da02aa5fefcce9d8f0235e
loreliawand <loreliawand@gmail.com>
2021-07-08 09:43:39 +0200

    Fixing

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.end('Future is coming');
+  res.send('Future is coming');
 });
 
 app.listen(port, () => {
