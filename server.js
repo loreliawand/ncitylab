c9abe4fb12237771e5aaac7c63ea77abdc4fd41f
loreliawand <loreliawand@gmail.com>
2021-07-31 23:10:51 +0200

    Added a path

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Handlebars = require('handlebars');
+const path = require('path');
+
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send('Future is coming');
+  res.sendFile(path.join(__dirname+'/dashboard.html'));
 });
 
 app.listen(port, () => {
