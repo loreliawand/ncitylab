3af511a684a362fb6b37853407f2722523206a79
loreliawand <loreliawand@gmail.com>
2021-08-20 08:36:59 +0200

    Added modules for reg, authetic and autorisat and conn to DB

--- server.js
+++ server.js
@@ -5,11 +5,34 @@ const bodyParser = require('body-parser');
 const Handlebars = require('handlebars');
 const path = require('path');
 const router = require('./routes');
-const db = require('./db');
+const MongoClient = require("mongodb").MongoClient;
+const mongoClient = new MongoClient("mongodb://localhost:27017/", { useUnifiedTopology: true });
+const mongoose = require('mongoose');
+const User = require("./user");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const port = process.env.PORT || 3000;
 
+const secret = "it's a wonderful idea!";
+
 app.set("wiev engine", "hbs");
+
+mongoClient.connect(function(err, client){
+
+    const db = client.db("ncitylab");
+    const collection = db.collection("users");
+});
+
+mongoose
+ .connect("mongodb://localhost:27017/ncitylab", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,   })
+ .then(() => console.log("Database connected!"))
+ .catch(err => console.log(err));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,6 +41,13 @@ app.use('/', router);
 app.use('/signup', router);
 app.use('/signin', router);
 
+app.use(session({
+  secret: secret,
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost:27017/ncitylab'
+  })
+}));
 
 app.use(function(req, res, next) {
   const err = new Error("Sorry, I didn't find anything");
