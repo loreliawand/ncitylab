6a29092fecbab22c426c527ff09dd55c7ea4ef69
loreliawand <loreliawand@gmail.com>
2021-08-26 19:31:14 +0200

    Changed file for veryfing token

--- auth.js
+++ auth.js
@@ -4,21 +4,21 @@ const mongoClient = new MongoClient("mongodb://localhost:27017/", { useUnifiedTo
 const mongoose = require('mongoose');
 const User = require("./user");
 const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
-const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
-
-  if (!token) {
-    return res.status(403).send("A token is required for authentication");
+const authenticateMiddleware = (req, res, next) => {
+  if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+    token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, secret, (err, user) => {
+      if (err) {
+          return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+  });
+  } else {
+    res.sendStatus(401);
   }
-  try {
-    const decoded = jwt.verify(token, secret);
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send("Invalid Token");
-  }
-  return next();
 };
 
-module.exports = verifyToken;
+module.exports = authenticateMiddleware;
