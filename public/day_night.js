7881a60e08daa54bbd8366f271d49e7af45cc68e
loreliawand <loreliawand@gmail.com>
2021-12-11 13:49:59 +0100

    Decorated switch

--- public/day_night.js
+++ public/day_night.js
@@ -12,11 +12,14 @@ function change (){
   if (href === "day.css") {
     document.querySelector("link[href='day.css']").href="night.css";
     href = "night.css";
+    document.querySelector("img[src='/img/day_sun.png']").src="/img/night_sun.png";
+
   }
 
   else {
     document.querySelector("link[href='night.css']").href="day.css";
     href = "day.css";
+    document.querySelector("img[src='/img/night_sun.png']").src="/img/day_sun.png";
   }
 
   //theme.setAttribute("href", href);
