a95671196eb22d03a01ccc072e14a3f8d80d53f5
loreliawand <loreliawand@gmail.com>
2021-12-13 08:39:12 +0100

    Added page with timer

--- routes.js
+++ routes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next)=>{
-    res.render('dashboard.pug');
+    res.render('dashboard.pug', { title: 'Dashboard' });
 });
+
+router.get('/timer', (req, res, next)=>{
+    res.render('timer.pug', { title: 'Until New Year...' });
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', router);
+app.use('/timer', router);
 
 app.use(function(req, res, next) {
   const err = new Error("Sorry, I didn't find anything");
