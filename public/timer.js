1f68a5f0b4e4f2522a81909ed7d3c7fe5d0577c3
loreliawand <loreliawand@gmail.com>
2021-12-13 07:16:23 +0100

    Added timer

---
+++ public/timer.js
@@ -0,0 +1,29 @@
+var endDate = new Date("Jan 01, 2022 00:00:00").getTime();
+var timer = setInterval(function() {
+    let now = new Date().getTime();
+    let t = endDate - now;
+
+    if (t >= 0) {
+
+        let days = Math.floor(t / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+        let secs = Math.floor((t % (1000 * 60)) / 1000);
+
+        document.getElementById("timer-days").innerHTML = days +
+        "<span class='label'>:</span>";
+
+        document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2)
+        + "<span class='label'>:</span>";
+
+        document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2)
+        + "<span class='label'>:</span>";
+
+        document.getElementById("timer-secs").innerHTML = ("0"+secs).slice(-2);
+
+    } else {
+        document.getElementById("timer").innerHTML = "Time is over!";
+
+    }
+
+}, 1000);
