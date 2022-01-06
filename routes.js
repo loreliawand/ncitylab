98b5e64e2dc81455938322d0f85c0d9ca297a1f9
loreliawand <loreliawand@gmail.com>
2022-01-06 21:18:54 +0100

    Added saving data from user

--- routes.js
+++ routes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+const Schema = mongoose.Schema;
+const User = require('./schema');
 
 router.get('/', (req, res, next)=>{
     res.render('dashboard.pug', { title: 'Dashboard' });
@@ -10,7 +14,19 @@ router.get('/timer', (req, res, next)=>{
 });
 
 router.get('/form', (req, res, next)=>{
+  let users = User.find({});
   res.render('form.pug', { title: 'User form' });
+});
+
+router.post('/form', (req, res)=>{
+  var myData = new User(req.body);
+  myData.save()
+    .then(item => {
+      res.send("item saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
 });
 
 
