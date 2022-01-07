771574423409345a48348697054d65f871866eea
loreliawand <loreliawand@gmail.com>
2022-01-07 20:06:15 +0100

    Checking unique data i making requiring in form

--- schema.js
+++ schema.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
-  Name: String,
-  Age: Number
+  Name: {type: String, required: true, unique: true},
+  Age: {type: Number, required: true}
 });
 
 const User = mongoose.model('User', userSchema);
