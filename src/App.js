98e8d1561c80927a0daf3a1b18c2874d3abf4ae6
loreliawand <loreliawand@gmail.com>
2022-02-17 16:31:52 +0100

    Work with component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+const App = () => {
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log('Hello from component. All systems are working normally');
+  return (
+    <div>
+      <p>Hello world, my name is Lora, and it is {now.toString()}</p>
+      <p>
+        Test: {a} + {b} = {a + b}
+      </p>
+    </div>
+  );
+};
 
 export default App;
