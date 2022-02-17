531a5d6ecfcc7325dabf440fb4f61857ae9b4646
loreliawand <loreliawand@gmail.com>
2022-02-17 16:46:41 +0100

    Trying a different component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-const App = () => {
+const Hello = () => {
   const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log('Hello from component. All systems are working normally');
   return (
     <div>
       <p>Hello world, my name is Lora, and it is {now.toString()}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const a = 10;
+  const b = 20;
+  console.log('All systems are working normally. Welcome');
+  return (
+    <div>
+      <h1>Greetings!</h1>
+      <Hello />
       <p>
         Test: {a} + {b} = {a + b}
       </p>
