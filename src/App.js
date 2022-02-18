70c88e8456a1c2e34a092742ddaf81f1c2fd211a
loreliawand <loreliawand@gmail.com>
2022-02-18 11:53:52 +0100

    Divided components

--- src/App.js
+++ src/App.js
@@ -16,24 +16,36 @@ const Hello = ({ name, age }) => {
   );
 };
 
+const CountSeconds = (props) => {
+  return <div>You didn't refresh this page {props.seconds} seconds :)</div>;
+};
+
+const Counter = (props) => {
+  return <div>Result of clicking is {props.counter} now :)</div>;
+};
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
+
 const App = () => {
   const [seconds, countSeconds] = useState(0);
   const [counter, setCounter] = useState(0);
   setTimeout(() => countSeconds(seconds + 1), 1000);
   const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
   const setToZero = () => setCounter(0);
   return (
     <div>
       <h1>Greetings!</h1>
       <Hello />
       <h1>Counter</h1>
+      <CountSeconds seconds={seconds} />
       <div>
-        <p>You didn't refresh this page {seconds} seconds :)</p>
-      </div>
-      <div>
-        <p>You clicked on a beautiful button {counter} times :)</p>
-        <button onClick={increaseByOne}>I am a beautiful button :)</button>
-        <button onClick={setToZero}>Reset! &gt;:(</button>
+        <Counter counter={counter} />
+        <Button onClick={increaseByOne} text="I am increase by one :)" />
+        <Button onClick={decreaseByOne} text="I am decrease be one :)" />
+        <Button onClick={setToZero} text="Reset! &gt;:(" />
       </div>
     </div>
   );
