565d8352160cdb014c4b205d623c1c84122d6863
loreliawand <loreliawand@gmail.com>
2022-02-23 19:10:21 +0100

    Function for adding notes to database

--- src/App.js
+++ src/App.js
@@ -67,11 +67,12 @@ const App = (props) => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    axios.post('http://localhost:3001/notes', noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
+      setNewNote('');
+    });
   };
 
   const handleNoteChange = (event) => {
