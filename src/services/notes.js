4bb61f20120d022e02cda603222ae7ec80dd972a
loreliawand <loreliawand@gmail.com>
2022-02-23 20:08:19 +0100

    Fixed bug

--- src/services/notes.js
+++ src/services/notes.js
@@ -3,17 +3,17 @@ const baseURL = 'http://localhost:3001/notes';
 
 const getAll = () => {
   const request = axios.get(baseURL);
-  request.then((response) => response.data);
+  return request.then((response) => response.data);
 };
 
 const create = (newObject) => {
   const request = axios.post(baseURL, newObject);
-  request.then((response) => response.data);
+  return request.then((response) => response.data);
 };
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseURL}/${id}`, newObject);
-  request.then((response) => response.data);
+  return request.then((response) => response.data);
 };
 
 export default { getAll, create, update };
