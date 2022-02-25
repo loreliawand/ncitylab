ac7e7820917080d6b61d90f8d0a7eb9730bc2529
loreliawand <loreliawand@gmail.com>
2022-02-25 16:50:56 +0100

    Added middlewares

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(express.json());
+app.use(requestLogger);
+app.use(unknownEndpoint);
+
 let notes = [
   {
     id: 1,
@@ -38,6 +54,28 @@ app.get('/api/notes/:id', (request, response) => {
     return note.id === id;
   });
   console.log(note);
+  response.json(note);
+});
+
+const generateID = () => {
+  const maxID = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+};
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: 'content missing' });
+  }
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateID(),
+  };
+
+  notes = notes.concat(note);
+
   response.json(note);
 });
 
