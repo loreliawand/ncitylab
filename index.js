5bda069a42719571389ad66d9fec4d180cff4c2b
loreliawand <loreliawand@gmail.com>
2022-02-25 16:06:22 +0100

    Added route to single note

--- index.js
+++ index.js
@@ -31,6 +31,16 @@ app.get('/api/notes', (request, response) => {
   response.json(notes);
 });
 
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => {
+    console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+    return note.id === id;
+  });
+  console.log(note);
+  response.json(note);
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
