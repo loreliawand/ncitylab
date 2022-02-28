aae1728efcce63579580cfabf8dc26bc0e8b2433
loreliawand <loreliawand@gmail.com>
2022-02-28 11:34:10 +0100

    Fixed bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios';
 
-axios.get('http://localhost:3001/notes').then((response) => {
+axios.get('http://localhost:3001/api/notes').then((response) => {
   ReactDOM.render(<App />, document.getElementById('root'));
 });
