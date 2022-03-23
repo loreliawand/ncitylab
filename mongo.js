5b435f54a0a5af4b2d4ecdea2b283ec13b31905c
loreliawand <loreliawand@gmail.com>
2022-03-23 20:59:08 +0100

    Deleted not important things

--- mongo.js
+++ mongo.js
@@ -20,21 +20,3 @@ const noteSchema = new mongoose.Schema({
 });
 
 const Note = mongoose.model('Note', noteSchema);
-
-const note = new Note({
-  content: 'I am back!',
-  date: new Date(),
-  important: false,
-});
-
-note.save().then((result) => {
-  console.log('note saved!');
-  mongoose.connection.close();
-});
-
-Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
