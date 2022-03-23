6dfdadb6fa4d156a1879caf04dea871ad8e8a8c4
loreliawand <loreliawand@gmail.com>
2022-03-23 20:56:58 +0100

    Deleted not important things

--- mongo.js
+++ mongo.js
@@ -31,3 +31,10 @@ note.save().then((result) => {
   console.log('note saved!');
   mongoose.connection.close();
 });
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
