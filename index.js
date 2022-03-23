75400414140ffd3aed6594c8d263008fc3a6ea66
loreliawand <loreliawand@gmail.com>
2022-03-23 21:13:38 +0100

    Removed version field from displaying notes

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 });
 
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 const Note = mongoose.model('Note', noteSchema);
 
 app.use(express.json());
