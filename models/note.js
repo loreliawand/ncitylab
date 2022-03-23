4433249e9b4028ca3c77ffaca81f1103dc588a89
loreliawand <loreliawand@gmail.com>
2022-03-23 22:02:54 +0100

    Fixed bug

--- models/note.js
+++ models/note.js
@@ -13,7 +13,7 @@ mongoose
     console.log('error connecting to MongoDB Atlas:', error.message);
   });
 
-const noteSchema = new Mongoose.schema({
+const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
