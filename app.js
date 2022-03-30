553baa36c423e2214461a4fae7518bfeeab9b96d
loreliawand <loreliawand@gmail.com>
2022-03-30 18:31:11 +0200

    Created main file

---
+++ app.js
@@ -0,0 +1,31 @@
+const app = express();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const notesRouter = require('./controllers/notes');
+const congif = require('./utils/config');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/notes', notesRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
