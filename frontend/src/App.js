317b0aeaebca3ecd49e33da0a163ddceae3e3c7f
loreliawand <loreliawand@gmail.com>
2022-04-23 13:12:22 +0200

    Deleted unusefull code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import './index.css';
-import Button from './components/Button';
-import Counter from './components/Counter';
-import CountSeconds from './components/CountSeconds';
 import Footer from './components/Footer';
 import Hello from './components/Hello';
-import History from './components/History';
 import LoginForm from './components/LoginForm';
 import Note from './components/Note';
 import NoteForm from './components/NoteForm';
@@ -17,11 +13,6 @@ import loginService from './services/login';
 
 const App = () => {
   const [seconds, countSeconds] = useState(0);
-  const [counter, setCounter] = useState(0);
-  const [counterHistory, setCounterHistory] = useState([]);
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
   const [notes, setNotes] = useState([]);
   const [showAllNotes, setShowAllNotes] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
@@ -46,36 +37,6 @@ const App = () => {
   }, []);
 
   setTimeout(() => countSeconds(seconds + 1), 1000);
-
-  const increaseByOne = () => {
-    setCounterHistory(counterHistory.concat('+ 1'));
-    setCounter(counter + 1);
-  };
-  const decreaseByOne = () => {
-    setCounterHistory(counterHistory.concat('- 1'));
-    setCounter(counter - 1);
-  };
-  const setToZero = () => {
-    setCounterHistory(counterHistory.concat('0'));
-    setCounter(0);
-  };
-
-  const clearClickingHistory = () => {
-    setCounterHistory([]);
-  };
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'));
-    setLeft(left + 1);
-  };
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    setRight(right + 1);
-  };
-
-  const clearAllHistory = () => {
-    setAll([]);
-  };
 
   const addNote = (noteObject) => {
     noteFormRef.current.toggleVisibility();
@@ -163,26 +124,6 @@ const App = () => {
         )}
       </div>
 
-      <div className="right lightgreen">
-        <h1>Counter</h1>
-        <Counter counter={counter} />
-        <Button handleClick={increaseByOne} text="I am increase by one :)" />
-        <Button handleClick={decreaseByOne} text="I am decrease be one :)" />
-        <Button handleClick={setToZero} text="Reset! &gt;:(" />
-        <History allClicks={counterHistory} />
-        <Button handleClick={clearClickingHistory} text="Clear history" />
-      </div>
-
-      <div className="center">
-        <h1>Left and right</h1>
-        {left}
-        <Button handleClick={handleLeftClick} text="I am a left button :)" />
-        <Button handleClick={handleRightClick} text="I am a right button :)" />
-        {right}
-        <History allClicks={allClicks} />
-        <Button handleClick={clearAllHistory} text="Clear history" />
-      </div>
-
       <div className="lightgreen">
         <h1>Notes</h1>
         <Notifications message={errorMessage} />
@@ -202,10 +143,6 @@ const App = () => {
             />
           ))}
         </ul>
-      </div>
-
-      <div>
-        <CountSeconds seconds={seconds} />
       </div>
 
       <div className="right">
