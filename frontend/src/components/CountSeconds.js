bfb9f932e04424869502cb4fe8d60cfa6c580a28
loreliawand <loreliawand@gmail.com>
2022-04-28 19:32:42 +0200

    Cleared project

--- frontend/src/components/CountSeconds.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CountSeconds = ({ seconds }) => (
-  <div>You didn't refresh this page {seconds} seconds :)</div>
-);
-
-export default CountSeconds;
--- frontend/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Footer = () => {
-  return <p>App by Lorelia Wand, 2022</p>;
-};
-
-export default Footer;
