7f48ff58c5ce9eb28fd4628565f1d6d22584879c
loreliawand <loreliawand@gmail.com>
2022-04-29 22:07:21 +0200

    Added another languages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const Footer = () => {
   return (
     <div className="footer">
@@ -7,15 +9,25 @@ const Footer = () => {
 };
 
 const Future = () => {
+  const [title, setTitle] = useState('Future is coming');
   return (
     <div>
-      <h1>Future is coming</h1>
+      <h1>{title}</h1>
+      <div className="buttons">
+        <button onClick={() => setTitle('Future is coming')}>English</button>
+        <button onClick={() => setTitle('Nadchodzi przyszlość')}>Polski</button>
+        <button onClick={() => setTitle('Будущее близко')}>Русский</button>
+        <button onClick={() => setTitle('Майбутнє надходить')}>
+          Українська
+        </button>
+      </div>
     </div>
   );
 };
 
 const App = () => {
   console.log('All systems are working normally');
+
   return (
     <div>
       <Future />
