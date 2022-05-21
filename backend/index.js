f97cbbcb9e87a0f3a154b595ef6cd1a1646e50a6
loreliawand <loreliawand@gmail.com>
2022-05-21 09:13:44 +0200

    Created server file and added posts

---
+++ backend/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+
+let posts = [
+  {
+    id: 1,
+    postHeader: 'Post in English',
+    postContent:
+      "This post was written by Me (link will be added later) in English. It's just a sample :)",
+    postDate: '03.05.2022, 15:18',
+  },
+  {
+    id: 2,
+    postHeader: 'Wpis po polsku',
+    postContent:
+      'Ten post był napisany Mną (link będzie dołączony pózniej) po polsku. To tylko przykład :)',
+    postDate: '03.05.2022, 15:20',
+  },
+  {
+    id: 3,
+    postHeader: 'Пост на русском',
+    postContent:
+      'Этот пост был написан Мной (ссылка будет добавлена позже) на русском. Это лишь пример :)',
+    postDate: '03.05.2022, 15:23',
+  },
+  {
+    id: 4,
+    postHeader: 'Пост українською',
+    postContent:
+      'Цей пост був написаний Мною (посилання буде додано пізніше) українською. Це лише приклад :)',
+    postDate: '03.05.2022, 15:25',
+  },
+];
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/posts', (request, response) => {
+  response.json(posts);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
